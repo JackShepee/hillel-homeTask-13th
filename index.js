f57b0e3f1e13ad4595f6c2793a6d04184651c2c3
JackShepee <salamakhaviktor@gmail.com>
2023-06-12 22:22:07 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+function create2DArray(outerSize, innerSize, value) {
+  // Валідація вхідних даних
+  if (
+    typeof outerSize !== "number" ||
+    typeof innerSize !== "number" ||
+    outerSize <= 0 ||
+    innerSize <= 0 ||
+    Math.floor(outerSize) !== outerSize ||
+    Math.floor(innerSize) !== innerSize
+  ) {
+    console.log("Size inputs must be positive integers");
+  }
+
+  return Array(outerSize)
+    .fill()
+    .map(() => Array(innerSize).fill(value));
+}
+
+let array = create2DArray(2, 4, "hillel");
+console.log(array);
